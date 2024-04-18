751b428e200bc3b96facc0c60a7e36a65c518f11
DucT4 <ductu0831@gmail.com>
2024-04-18 12:34:29 +0700

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,11 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
+		System.out.println("Hello World");
 	}
 
 }
